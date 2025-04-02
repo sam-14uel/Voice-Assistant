7ce22324ec6c6a07a9fab55e51ef15147a5c20e2
sam-14uel <sammyfirst6@gmail.com>
2025-04-02 11:38:13 +0100

    updated requirements.txt

--- AI_Agent_Config/deployment.py
+++ AI_Agent_Config/deployment.py
@@ -33,7 +33,7 @@ CSRF_TRUSTED_ORIGINS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
+    # 'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
